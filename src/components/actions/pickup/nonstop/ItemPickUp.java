27aab2b02321791e4a7078caba8ca2abc684f685
Minh Kiên <20020170@vnu.edu.vn>
2022-10-03 19:47:44 +0700

    update components

---
+++ src/components/actions/pickup/nonstop/ItemPickUp.java
@@ -0,0 +1,31 @@
+package components.actions.pickup.nonstop;
+
+import components.actions.collide.Collision;
+import components.actions.collide.CollisionAction;
+import components.actions.collide.entity.ItemCollision;
+import components.actions.pickup.PickUp;
+import components.actions.pickup.PickUpDecorator;
+import components.entities.statics.items.Item;
+
+public class ItemPickUp extends PickUpDecorator {
+
+    private Collision collision;
+
+    public ItemPickUp(PickUp pickUp) {
+        super(pickUp);
+
+        this.collision = new CollisionAction(pickUp.getEntity());
+        this.collision = new ItemCollision(collision);
+    }
+
+    @Override
+    protected void decorate() {
+        if (collision.isCollided()) {
+            collision.getCollidedEntities().forEach(item -> {
+                ((Item) item).boost((Player) getEntity());
+            });
+
+            collision.clearCollidedEntities();
+        }
+    }
+}
